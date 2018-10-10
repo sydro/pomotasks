61308ddbabb7421861ff5edc3c0b23d4eed3d2df
Angelo D'Autilia <angelo.dautilia@alcacoop.it>
2018-10-10 17:08:44 +0200

    Added badge with tasks number

--- components/ItemCategory.js
+++ components/ItemCategory.js
@@ -1,8 +1,19 @@
 import React from 'react'
-import { Icon, CheckBox, ListItem, Text, Left, Right, Input, Button } from 'native-base'
+import { Icon, ListItem, Text, Left, Right, Button, Badge } from 'native-base'
 
 export default class Category extends React.Component {
+  renderBadge() {
+    const numTasks = this.props.cat.tasks.length
+    if (numTasks !== 0) {
+      return (
+        <Badge success style={{ height: 24, top: 5 }}>
+          <Text>{numTasks}</Text>
+        </Badge>
+      )
+    } else return null
+  }
   render() {
+    const badge = this.renderBadge()
     return (
       <ListItem>
         <Left>
@@ -12,6 +23,7 @@ export default class Category extends React.Component {
               this.props.onNavigate(this.props.cat)
             }}>
             <Text>{this.props.cat.name}</Text>
+            {badge}
           </Button>
         </Left>
         <Right>
